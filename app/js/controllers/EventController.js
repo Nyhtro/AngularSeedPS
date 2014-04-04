64f784c845a1dd1d2eeba37d951dd002adc32aeb
Trond Martin Nyhus <nyhtro@gmail.com>
2014-04-04 10:31:00 +0200

    Initial commit

---
+++ app/js/controllers/EventController.js
@@ -0,0 +1,52 @@
+'use strict';
+
+eventsApp.controller('EventController',
+    function EventController($scope) {
+
+        $scope.event = {
+            name: 'Angular Boot Camp',
+            date: '1/1/2013',
+            time: '10:30 am',
+            location: {
+                address: 'Shibby',
+                city: "Shabby",
+                province: "Shobby"
+            },
+            imageUrl: 'img/AngularJs-large.png',
+            sessions: [
+                {
+                    name: 'Super sessions',
+                    creatorName: "Bobby",
+                    duration: '1 hr',
+                    level: 'Super',
+                    abstract: 'Learning about shibby stuff',
+                    upVoteCount: 0
+                },
+                {
+                    name: 'Shibby session',
+                    creatorName: "Bobby",
+                    duration: '1 hr',
+                    level: 'Super',
+                    abstract: 'Learning about shibby stuff',
+                    upVoteCount: 0
+                },
+                {
+                    name: "Average",
+                    creatorName: "Bobby",
+                    duration: '1 hr',
+                    level: 'Super',
+                    abstract: 'Learning about shibby stuff',
+                    upVoteCount: 0
+                }
+            ]
+        }
+
+        $scope.upVoteSession = function(session) {
+            session.upVoteCount++;
+        }
+
+        $scope.downVoteSession = function(session) {
+            session.upVoteCount--;
+        }
+    }
+);
